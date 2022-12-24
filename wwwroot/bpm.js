0b75140acd8415f34aa9acfc0131ed18efab05e6
Andrei Baltushkin <andreyboltushkin2002@gmail.com>
2022-12-23 17:43:01 -0700

    added major buttons dynamic underline

--- wwwroot/bpm.js
+++ wwwroot/bpm.js
@@ -2,8 +2,7 @@ async function LoadProjects(projectsPath) {
     try {
         document.querySelector("#projects-list").innerHTML = "";
         const projects = await fetch(projectsPath, {mode: 'no-cors'}).then(response => response.json())
-        console.log(projects);
-        // const projects = JSON.parse(data);
+        // console.log(projects);
         for (let i = 0; i < Object.keys(projects.projects).length; i ++)
         {
             let projectAtI = document.createElement("li");
@@ -21,19 +20,35 @@ function PrepareButtons()
     document.querySelector("#btn-projects-done").addEventListener("click", function() {LoadProjects("https://localhost:7161/bpm-main-info-done");});
     // document.querySelector("#btn-projects-current").addEventListener("click", function () {LoadProjects("Data/projects-current.json");});
     // document.querySelector("#btn-projects-done").addEventListener("click", function () {LoadProjects("Data/projects-done.json");});
-    
 }
 function UploadMainPage()
 {
     document.querySelector("main.major-main").innerHTML = `<nav id="projects-nav" class="projects">
-    <button type="button" class="projects" id="btn-projects-current"><svg class="text-underline" height="20" width="50"><line x1="0" y1="14" x2="45" y2="14" style="stroke:rgb(255,0,0);stroke-width:2" /></svg><span>Current</span></button>
-    <button type="button" class="projects" id="btn-projects-done"><span>Done<span></button>
+    <button type="button" class="projects" id="btn-projects-current"><svg class="text-underline" height="20" width="50"><line x1="0" y1="14" x2="0" y2="14" style="stroke:rgb(189,223,255);stroke-width:2" /></svg><span>Current</span></button>
+    <button type="button" class="projects" id="btn-projects-done"><svg class="text-underline" height="20" width="50"><line x1="0" y1="14" x2="0" y2="14" style="stroke:rgb(189,223,255);stroke-width:2" /></svg><span>Done<span></button>
 </nav>
 <ul id="projects-list"></ul>`;
-    document.querySelector("svg.text-underline").addEventListener("hover", () => {
-        let line = document.querySelector("svg.text-underline line");
-        console.log(line);
+    let projectButtons = document.querySelectorAll("button.projects")
+    console.log(projectButtons);
+    projectButtons.forEach((button) => {
+        button.addEventListener("mouseover", () => {
+            let line = button.firstChild.firstChild;
+            line.setAttribute("x2", "1");
+            line.style.transition = "all 0.5s";
+            line.style.transitionTimingFunction = "ease-in-out";
+            // scale size = size of the text inside a button
+            let scaleSize = button.lastChild.getBoundingClientRect().width;
+            line.style.transform = `scaleX(${scaleSize})`;
+        });
+        button.addEventListener("mouseout", () => {
+            let line = button.firstChild.firstChild;
+            line.setAttribute("x1", "0");
+            line.style.transform = "scaleX(0)";
+            line.style.transition = "all 0.5s";
+            line.style.transitionTimingFunction = "ease-in-out";
+        });
     });
+    
     PrepareButtons();
 }
 async function LoadEmptyJSON()
